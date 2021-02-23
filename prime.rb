7468af48ac1a1d4560d16748ba0b40a16f5b5422
julianxlozano <julianxlozano@gmail.com>
2021-02-23 16:52:26 -0600

    commit tree 2657137d0776f3fe08854e8c5042e60ac4b168fc

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
 # Add  code here!
+require 'pry'
+
+def prime?(num)
+
+    if num.negative? == true || num==0 || num==1
+        
+                return false
+                
+          
+    else
+
+ 
+    (2..(num - 1)).each do |n|
+        if num % n == 0 
+            
+            return false
+            
+      end
+    end
+end
+
+     true 
+
+end
+
+
+
